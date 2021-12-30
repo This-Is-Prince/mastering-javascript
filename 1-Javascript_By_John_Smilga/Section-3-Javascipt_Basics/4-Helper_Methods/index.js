742c24f1c9d0f0221f7c15f0b36bb0a71ba9d569
Prince <this.is.kumar.prince@gmail.com>
2021-12-30 15:06:00 +0530

    helpers methods completed

---
+++ 1-Javascript_By_John_Smilga/Section-3-Javascipt_Basics/4-Helper_Methods/index.js
@@ -0,0 +1,11 @@
+// document.write("hello world");
+// alert("hello world");
+// console.log("hello world");
+
+// window.document.write("hello world");
+// window.alert("hello people");
+// window.console.log("hello world");
+
+window.document.write({ name: "Prince" });
+window.alert({ name: "Prince" });
+window.console.log({ name: "Prince" });
