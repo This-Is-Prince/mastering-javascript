691360326b319fba4ffd5c306364ac7d69c8d459
Prince <this.is.kumar.prince@gmail.com>
2021-12-30 18:04:02 +0530

    Statements and comments completed

---
+++ 1-Javascript_By_John_Smilga/Section-3-Javascipt_Basics/5-Statements_and_Comments/index.js
@@ -0,0 +1,10 @@
+// console.log("Hello world"); // this line will not run.
+console.log("Hello people");
+console.log("Hello my friend peter");
+console.log("Hello susan");
+
+/**
+ * This is multi line comment
+ */
+
+// This is single line comment
