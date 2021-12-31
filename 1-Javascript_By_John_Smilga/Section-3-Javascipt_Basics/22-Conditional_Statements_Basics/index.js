a1a6fe103b76be6f29a099c5621df38a0cb67f85
Prince <this.is.kumar.prince@gmail.com>
2021-12-31 09:10:56 +0530

    conditional statements completed

---
+++ 1-Javascript_By_John_Smilga/Section-3-Javascipt_Basics/22-Conditional_Statements_Basics/index.js
@@ -0,0 +1,22 @@
+// Conditional Statements
+// >, <, >=, <=, ==, ===, !=,!==
+
+if (true) {
+  console.log("hello world");
+}
+
+if (2 > 1) {
+  console.log("2 is bigger than 1");
+}
+
+const value = true;
+if (value) {
+  console.log("value is " + value);
+} else {
+  console.log("value is " + value);
+}
+
+const cond = 2 > 1;
+if (cond) {
+  console.log("value of cond is " + cond);
+}
---
+++ 1-Javascript_By_John_Smilga/Section-3-Javascipt_Basics/23-Conditional_Statements_Continued/index.js
@@ -0,0 +1,16 @@
+// Conditional Statements
+// Comparison Operators
+// >, <, >=, <=, ==, ===, !=, !==
+// else if and !
+
+const num1 = 6;
+const num2 = 6;
+
+const result = num1 >= num2;
+if (num1 > num2) {
+  console.log("first number is bigger than second");
+} else if (result) {
+  console.log("first number equal to a second");
+} else {
+  console.log("second number is bigger than first");
+}
